2d9c4c009ad3830cfb4ab59478e18d76b7f5fd86
Hansenkylem@gmail.com <Hansenkylem@gmail.com>
2020-10-11 13:38:37 -0400

    Mission 3 S2 - The Robotic Systems

--- Finch_Starter/Program.cs
+++ Finch_Starter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FinchAPI;
 using System;
+using System.Linq;
 
 namespace Finch_Starter
 {
@@ -12,12 +13,12 @@ namespace Finch_Starter
         //                      with the Finch robot via the Finch API.
         // Author:              Kyle Hansen
         // Date Created:        09/30/2020
-        // Date Modified:       10/04/2020        
+        // Date Modified:       10/11/2020        
         // *************************************************************
 
         static void Main(string[] args)
         {
-          
+
             //
             // create a new Finch object
             //
@@ -96,7 +97,7 @@ namespace Finch_Starter
         static bool DisplayConnectFinchRobot(Finch finchRobot)
         {
             string userInput;
-            bool connected =false;
+            bool connected = false;
             bool connectedLoop = false;
             DisplayHeader("The application will attempt to connect to the finch robot. Please hit any key to contunue.");
             DisplayContinuePrompt();
@@ -114,7 +115,7 @@ namespace Finch_Starter
                     DisplayHeader("The connection failed. Make sure the robot is connected. Do you wish to try agian(y or n)?");
                     userInput = Console.ReadLine().ToLower();
                     // Keep attempting connection loop unless told not to
-                    if(userInput != null && userInput == "n")
+                    if (userInput != null && userInput == "n")
                     {
                         connectedLoop = true;
                     }
@@ -136,14 +137,191 @@ namespace Finch_Starter
             return true;
         }
         //
-        // This method is the data recorder menu. It is under development.
+        // This method is the data recorder menu. 
         //
         static void DataRecorderDisplayMenuScreen(Finch finchRobot)
         {
-            DisplayHeader("The Data Recoder is under development. Please hit any key to contunue.");
+
+            bool menuloop = true;
+            string userInput;
+            int numberOfDataPoints = 0;
+            Double dataPointFrequency = 0;
+            Double[] temperatures = null;
+
+            do
+            {
+                DisplayHeader("Data Recorder Menu..Please Select from the following options:");
+                Console.WriteLine("\t\t*******************************************");
+                Console.WriteLine();
+                Console.WriteLine("\t\t1. Number of Data Points");
+                Console.WriteLine("\t\t2. Frequency of Data Points");
+                Console.WriteLine("\t\t3. Get Data");
+                Console.WriteLine("\t\t4. Show Data");
+                Console.WriteLine("\t\t5. Return to Main Menu");
+                Console.WriteLine();
+                Console.WriteLine("\t\t*******************************************");
+                userInput = Console.ReadKey().Key.ToString().Substring(1, 1);
+                switch (userInput)
+                {
+                    case "1":
+                        numberOfDataPoints = DataRecorderDisplayGetNumberOfDataPoints();
+                        break;
+                    case "2":
+                        dataPointFrequency = DataRecorderDisplayGetDataPointFrequency();
+                        break;
+                    case "3":
+                        temperatures = DataRecorderDisplayGetData(numberOfDataPoints, dataPointFrequency, finchRobot);
+                        break;
+                    case "4":
+                        DataRecorderDisplayData(temperatures);
+                        break;
+                    case "5":
+                        menuloop = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (menuloop);
+        }
+        //
+        // This method gets the frequency of data points. 
+        //
+        static double DataRecorderDisplayGetDataPointFrequency()
+        {
+            double returnValue;
+            string userValue;
+            bool isNumber = false;
+            // Validate user input string via a bool and a loop. If false stay in loop. If a number leave the loop.
+            do
+            {
+                DisplayHeader("Please enter the frequency of the readings in seconds:");
+                userValue = Console.ReadLine();
+                isNumber = double.TryParse(userValue, out returnValue);
+            }
+            while (isNumber == false);
+            Console.WriteLine();
+            Console.WriteLine("You have selected the frequency of: " + returnValue);
+            Console.WriteLine("Please hit any key to contunue.");
             DisplayContinuePrompt();
+            return returnValue;
         }
 
+        //
+        // This method lets the user select the number of data points
+        //
+        static int DataRecorderDisplayGetNumberOfDataPoints()
+        {
+            int returnValue;
+            string userValue;
+            bool isNumber = false;
+            // Validate user input string via a bool and a loop. If false stay in loop. If a number leave the loop.
+            do
+            {
+                DisplayHeader("Please enter the number of data points:");
+                userValue = Console.ReadLine();
+                isNumber = int.TryParse(userValue, out returnValue);
+            }
+            while (isNumber == false);
+            Console.WriteLine();
+            Console.WriteLine("You have selected the following number of data points: " + returnValue);
+            Console.WriteLine("Please hit any key to contunue.");
+            DisplayContinuePrompt();
+            return returnValue;
+        }
+
+        //
+        // This method gets data(Temperature, right light sensor value, left light sensor value)
+        //
+        static double[] DataRecorderDisplayGetData(int numberOfDataPoints, double dataPointFrequency, Finch finchRobot)
+        {
+            double[] returnArray = new double[numberOfDataPoints];
+            int[] rightLight = new int[numberOfDataPoints];
+            int[] leftLight = new int[numberOfDataPoints];
+            int leftLightSum;
+            int rightLightSum;
+            double tempAvg;
+            string userValue;
+
+
+
+            DisplayHeader("Get Data Screen");
+            Console.WriteLine("We will collect the folowing number of data points: " + numberOfDataPoints);
+            Console.WriteLine("For the following Frequency: " + dataPointFrequency);
+            Console.WriteLine("The program is ready to read in the data.");
+            Console.WriteLine("Please hit any key to contunue.");
+            Console.WriteLine("*******************************************************");
+            DisplayContinuePrompt();
+            Console.WriteLine("");
+            for (int arrayIndex = 0; arrayIndex < numberOfDataPoints; arrayIndex++)
+            {
+                returnArray[arrayIndex] = ConvertCelsiusToFahrenheit(finchRobot.getTemperature());
+                rightLight[arrayIndex] = finchRobot.getRightLightSensor();
+                leftLight[arrayIndex] = finchRobot.getLeftLightSensor();
+                Console.WriteLine("Reading " + (arrayIndex + 1) + " :" + (returnArray[arrayIndex]).ToString("n2") + " Right Light:" + rightLight[arrayIndex] + " Left Light:" + leftLight[arrayIndex]);
+                finchRobot.wait((int)(dataPointFrequency * 1000));
+            }
+            rightLightSum = rightLight.Sum();
+            leftLightSum = leftLight.Sum();
+            tempAvg = returnArray.Average();
+            Console.WriteLine("");
+            Console.WriteLine("The average Temperature:" + tempAvg.ToString());
+            Console.WriteLine("Right light sum of values:" + rightLightSum.ToString());
+            Console.WriteLine("Left light sum of value:" + leftLightSum.ToString());
+            Console.WriteLine("Right light Average Value:" + (rightLightSum / numberOfDataPoints).ToString());
+            Console.WriteLine("Left light Average Value:" + (leftLightSum / numberOfDataPoints).ToString());
+            Console.WriteLine("");
+            Console.WriteLine("The data recording is now complete.");
+
+            do
+            {
+                Console.WriteLine("Do you want to see a list of sorted light values? Please enter (y or n):");
+                userValue = Console.ReadLine().ToLower().Substring(0, 1);
+            }
+            while (userValue != "y" && userValue != "n");
+
+            if (userValue == "y")
+            {
+                Array.Sort(rightLight);
+                Array.Sort(leftLight);
+                Console.WriteLine("Right Value".PadLeft(15) + "Left Value".PadLeft(15));
+                Console.WriteLine("---------".PadLeft(15) + "-----------".PadLeft(15));
+                for (int i = 0; i < numberOfDataPoints; i++)
+                {
+                    Console.WriteLine(rightLight[i].ToString().PadLeft(15) + leftLight[i].ToString().PadLeft(15));
+                }
+
+            }
+            Console.WriteLine("Please hit any key to contunue.");
+            DisplayContinuePrompt();
+            return returnArray;
+        }
+
+        //
+        // This method converts a Celsius double into a ToFahrenheit double( we would not need this in Canada). 
+        //
+        static double ConvertCelsiusToFahrenheit(double celsiusTemp)
+        {
+            return (celsiusTemp * (1.8)) + 32;
+        }
+        static void DataRecorderDisplayData(double[] data)
+        {
+            DisplayHeader("Displaying data:");
+            Console.WriteLine("");
+            DataRecorderDisplayDataTable(data);
+            Console.WriteLine("Please hit any key to contunue.");
+            DisplayContinuePrompt();
+        }
+        static void DataRecorderDisplayDataTable(double[] data)
+        {
+            Console.WriteLine("Reading #".PadLeft(15) + "temperature".PadLeft(15));
+            Console.WriteLine("---------".PadLeft(15) + "-----------".PadLeft(15));
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine((i+ 1).ToString().PadLeft(15) + data[i].ToString("n2").PadLeft(15));
+            }
+        }
+
+        
         //
         // This method is the alarm system menu. It is under development.
         //
